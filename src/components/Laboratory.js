6397009f6d2cb7653f4a1153ee7f679ec017c933
Erin Van Brunt <erinmontybruce@gmail.com>
2022-05-26 14:49:24 -0700

    change item images background transparent

--- src/components/Laboratory.js
+++ src/components/Laboratory.js
@@ -1,9 +1,15 @@
 import React from "react";
+import crystal_egg from "../images/ingredients/crystalegg.png";
 
 function Laboratory() {
   return (
     <div className="lab-container">
-      <h1>Laboratory!!!</h1>
+      <h1>Laboratory</h1>
+      <div className="ingredients-container">
+        {/* ingredients mapped here */}
+        <img src={crystal_egg} alt="crystal egg" className="item" />
+      </div>
+      <div className="cauldron-container"></div>
     </div>
   );
 }
